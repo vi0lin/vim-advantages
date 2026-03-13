db20531980d4216d8cf09572c4fe09c98fd015da
vi0lin <codekowski@gmail.com>
2026-03-13 16:28:10 +0100

    Unsilent

--- install.sh
+++ install.sh
@@ -148,7 +148,7 @@ install() {
   fi
 
   echo "Installing Vim-Advantages (with plug.vim)"
-  $vimbinary -es -c "source ${plugins}plug.vim | call plug#begin() | Plug 'vi0lin/vim-advantages' | call plug#end() | PlugInstall | quitall"
+  $vimbinary -c "source ${plugins}plug.vim | call plug#begin() | Plug 'vi0lin/vim-advantages' | call plug#end() | PlugInstall | quitall"
 }
 
 install "vim"
